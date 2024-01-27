e7a93da008970e7d5218042e4acb18c5643c1220
VaibhavMore3 <vaibhavmore.b@gmail.com>
2024-01-28 01:02:57 +0530

    New

--- src/main/java/org/automation/modules/PayloadManager.java
+++ src/main/java/org/automation/modules/PayloadManager.java
@@ -38,6 +38,32 @@ public class PayloadManager {
 
     }
 
+    public Booking JsonToObjectPUT(String jsonString) throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+        Booking bookingResponse = objectMapper.readValue(jsonString, Booking.class);
+        return bookingResponse;
+    }
+
+    public String updatePayload() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+
+        Booking booking = new Booking();
+        booking.setFirstname("ABC");
+        booking.setLastname("XYZ");
+        booking.setAdditionalneeds("Lunch");
+        booking.setDepositpaid(true);
+        booking.setTotalprice(200);
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2018-01-01");
+        bookingdates.setCheckout("2018-01-02");
+
+        booking.setBookingdates(bookingdates);
+
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+    }
+
     public String setToken() throws JsonProcessingException {
         objectMapper = new ObjectMapper();
         Auth auth = new Auth();
--- src/test/java/org/automation/crud/TC_Integration.java
+++ src/test/java/org/automation/crud/TC_Integration.java
@@ -1,6 +1,13 @@
 package org.automation.crud;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.automation.base.BaseTest;
+import org.automation.endpoints.APIConstants;
+import org.automation.payloads.request.Booking;
+import org.automation.payloads.response.BookingResponse;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 import org.automation.base.BaseTest.*;
 import static org.assertj.core.api.Assertions.*;
@@ -10,26 +17,65 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TC_Integration extends BaseTest {
 
     String token;
+    String bookingID;
+    String bookingIDPOJO;
 
     @Test
     public void testCreateBooking() throws JsonProcessingException {
         token = getToken();
-        System.out.println(token);
-        assertThat("Vaibhav").isEqualTo("Vaibhav");
+        assertThat(token).isNotNull().isNotEmpty();
+
+        requestSpecification.baseUri(APIConstants.baseURL+APIConstants.basePath);
+        response = requestSpecification.when().body(payloadManager.createPayload()).post();
+        validatableResponse = response.then().log().all();
+        jsonPath = JsonPath.from(response.asString());
+        validatableResponse.statusCode(200);
+
+        // simple way -
+        bookingID = jsonPath.getString("bookingid");
+
+
+        //Creating a class (optional)
+        BookingResponse bookingResponse = payloadManager.JsonToObject(response.asString());
+        bookingIDPOJO = bookingResponse.getBookingid().toString();
+
 
     }
 
 
     @Test(dependsOnMethods = {"testCreateBooking"})
-    public void testCreateAndUpdateBooking(){
-        System.out.println(token);
-        assertThat("Vaibhav").isEqualTo("Vaibhav");
+    public void testCreateAndUpdateBooking() throws JsonProcessingException {
+
+        requestSpecification.basePath(APIConstants.updateBooking+"/"+bookingID);
+        response = RestAssured.given().spec(requestSpecification).cookie("token",token)
+                .when().body(payloadManager.updatePayload()).put();
+        validatableResponse = response.then().log().all();
+        //validatableResponse.body("firstname", Matchers.is("ABC"));
+
+        Booking bookingResponse = payloadManager.JsonToObjectPUT(response.asString());
+        assertThat(bookingResponse.getFirstname()).isEqualTo("ABC").isNotNull();
+        assertThat(bookingResponse.getLastname()).isEqualTo("XYZ").isNotNull();
+
+
     }
 
-    @Test (dependsOnMethods = {"testCreateBooking"})
+    @Test (dependsOnMethods = {"testCreateAndUpdateBooking"})
     public void testDeleteBooking(){
-        System.out.println(token);
-        assertThat("Vaibhav").isEqualTo("Vaibhav");
+
+        requestSpecification.basePath(APIConstants.updateBooking+"/"+bookingID).cookie("token",token);
+        validatableResponse = RestAssured.given().spec(requestSpecification).auth().basic("admin","password123")
+                .when().delete().then().log().all();
+        validatableResponse.statusCode(201);
+
+    }
+
+    @Test (dependsOnMethods = {"testDeleteBooking"})
+    public void testDeletedBookingByGET(){
+
+        requestSpecification.basePath(APIConstants.updateBooking+"/"+bookingID);
+        response = RestAssured.given().spec(requestSpecification).when().get();
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(404);
 
     }
 
